bd07d06a490c127735bfccc340ad48025f99903f
Andrea del Monaco <andrea.delmonaco@outlook.it>
2018-09-01 17:25:45 +0200

    w

--- test.py
+++ test.py
@@ -7,14 +7,18 @@ import aenum #2.1.2
 
 if __name__ == '__main__':
 
-    print("Package:\t{} {}".format(requests.__name__,requests.__version__));
+    f = open('version.txt', 'w')
+
+    f.writelines("- `{} >= {}`\n".format(requests.__name__,requests.__version__));
     
-    print("Package:\t{} {}".format(pandas.__name__,pandas.__version__));
+    f.write("- `{} >= {}`\n".format(pandas.__name__,pandas.__version__));
     
-    print("Package:\t{} {}".format(numpy.__name__,numpy.__version__));
+    f.write("- `{} >= {}`\n".format(numpy.__name__,numpy.__version__));
     
-    print("Package:\t{} {}".format(pytz.__name__,pytz.__version__));
+    f.write("- `{} >= {}`\n".format(pytz.__name__,pytz.__version__));
     
-    print("Package:\t{} {}".format(dateutil.__name__,dateutil.__version__));
+    f.write("- `{} >= {}`\n".format(dateutil.__name__,dateutil.__version__));
     
-    print("Package:\t{}".format(aenum));
+    f.write("- `{} >= {}`\n".format(aenum,'2.1.2'));
+
+    f.close()
