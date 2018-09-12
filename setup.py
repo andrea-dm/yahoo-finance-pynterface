ff7b06618c24c5248467d077a48a3840a640c15d
Andrea del Monaco <andrea.delmonaco@outlook.it>
2018-09-12 22:19:44 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yahoo_finance_pynterface",
-    version="1.0.1",
+    version="1.0.2",
     author="Andrea del Monaco",
     author_email="and.delmonaco@gmail.com",
     description="A Python Interface to the Yahoo Finance API",
@@ -14,7 +14,11 @@ setuptools.setup(
     url="https://github.com/andrea-dm/yahoo-finance-pynterface",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"],
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Financial and Insurance Industry",],
 )
